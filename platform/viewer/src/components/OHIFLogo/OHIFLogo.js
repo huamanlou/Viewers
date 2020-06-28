31b16d0f01d063424c423fc1d4eb1773046bfdc9
= <=>
2020-06-28 14:28:21 +0800

    ···

--- platform/viewer/src/components/OHIFLogo/OHIFLogo.js
+++ platform/viewer/src/components/OHIFLogo/OHIFLogo.js
@@ -11,7 +11,7 @@ function OHIFLogo() {
       className="header-brand"
       href="/"
     >
-      <img src="https://photo.zastatic.com/images/common-cms/it/20200628/1593313626305_353793_t.png" width="100" />
+      <img src="https://photo.zastatic.com/images/common-cms/it/20200628/1593313626305_353793_t.png" width="83" />
       {/* Logo text would fit smaller displays at two lines:
        *
        * Open Health
@@ -19,7 +19,7 @@ function OHIFLogo() {
        *
        * Or as `OHIF` on really small displays
        */}
-      Angel Soft
+      &nbsp;&nbsp;&nbsp; Angel Soft &nbsp;&nbsp;&nbsp;
     </a>
   );
 }
